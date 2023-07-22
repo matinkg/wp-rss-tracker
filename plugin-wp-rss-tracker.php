2fb2fbe12123ba967bcfd21d16e6f82a29c5e58a
Matin Kargar (Axone) <matinkargar13@gmail.com>
2023-07-22 14:58:50 +0330

    Add: settings db

--- plugin-wp-rss-tracker.php
+++ plugin-wp-rss-tracker.php
@@ -36,6 +36,31 @@ register_activation_hook(__FILE__, function () {
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
     }
+
+    // Define your table name
+    $table_name = $wpdb->prefix . 'rss_tracker_settings';
+
+    // Check if the table already exists
+    if ($wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name) {
+        // Table does not exist, create it
+        $charset_collate = $wpdb->get_charset_collate();
+
+        // Define your table structure
+        $sql = "CREATE TABLE {$table_name} (
+            id INT(11) NOT NULL AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            value TEXT DEFAULT NULL,
+            PRIMARY KEY (id)
+        ) $charset_collate;";
+
+        // Execute the SQL query
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        
+        // add setting values
+        $wpdb->insert($wpdb->prefix . 'rss_tracker_settings', array('name' => 'posts_expiration_time', 'value' => '',));
+    }
+
 });
 
 function wprt_uninstall_plugin()
@@ -74,11 +99,24 @@ add_action('admin_menu', function () {
             include_once(plugin_dir_path(__FILE__) . 'controllers/add-rss.php');
         }
     );
+
+    /* settings */
+    add_submenu_page(
+        'rss-list',
+        'Settings',
+        'Settings',
+        'manage_options',
+        'settings',
+        function () {
+            include_once(plugin_dir_path(__FILE__) . 'controllers/settings.php');
+        }
+    );
 });
 
 /* ---------------- Custom link for posts added by the plugin --------------- */
 // Filter the post permalink
-function custom_post_permalink($permalink, $post) {
+function custom_post_permalink($permalink, $post)
+{
     // Check if the post has a rss link
     $rss_link = get_post_meta($post->ID, 'rss_link', true);
     if (!empty($rss_link)) {
@@ -92,54 +130,57 @@ add_filter('post_type_link', 'custom_post_permalink', 10, 2);
 
 /* --------------- Custom title for posts added by the plugin --------------- */
 // Add a custom tag before the wp-block-post-title block
-function add_custom_tag_before_post_title( $block_content, $block ) {
+function add_custom_tag_before_post_title($block_content, $block)
+{
     global $post;
 
-    if ($post && isset($post->ID) && 'core/post-title' === $block['blockName'] ) {
+    if ($post && isset($post->ID) && 'core/post-title' === $block['blockName']) {
         $post_id = $post->ID;
 
         /* check if rss_source meta exist */
         $rss_source = get_post_meta($post_id, 'rss_source', true);
         if (!empty($rss_source)) {
-            $custom_tag = '<span class="rss-src">'.$rss_source.'</span>';
-        
+            $custom_tag = '<span class="rss-src">' . $rss_source . '</span>';
+
             // Append the custom tag with post meta to the block content
             $block_content = $custom_tag . $block_content;
         }
-
     }
 
     return $block_content;
 }
-add_filter( 'render_block', 'add_custom_tag_before_post_title', 10, 2 );
+add_filter('render_block', 'add_custom_tag_before_post_title', 10, 2);
 
 
 /* --------------------------------- Cronjob -------------------------------- */
 // Schedule the cron event when the plugin is activated
 register_activation_hook(__FILE__, 'wprt_activate');
-function wprt_activate() {
+function wprt_activate()
+{
     wp_schedule_event(time(), 'every_15_minutes', 'wprt_function');
 }
 
 // Unschedule the cron event when the plugin is deactivated
 register_deactivation_hook(__FILE__, 'wprt_deactivate');
-function wprt_deactivate() {
+function wprt_deactivate()
+{
     wp_clear_scheduled_hook('wprt_function');
 }
 
 // Function to run every 15 minutes
 add_action('wprt_function', 'wprt_do_something');
-function wprt_do_something() {
+function wprt_do_something()
+{
     include_once(plugin_dir_path(__FILE__) . 'cron.php');
 }
 
 // Add custom cron interval for every 15 minutes
 add_filter('cron_schedules', 'wprt_custom_cron_interval');
-function wprt_custom_cron_interval($schedules) {
+function wprt_custom_cron_interval($schedules)
+{
     $schedules['every_15_minutes'] = array(
         'interval' => 900, // 15 minutes in seconds
         'display'  => __('Every 15 Minutes')
     );
     return $schedules;
 }
-
