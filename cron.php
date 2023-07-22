bafc56e26f9dcf8143a5bb5a9387915d78cacd03
Matin Kargar (Axone) <matinkargar13@gmail.com>
2023-07-22 16:12:27 +0330

    Minor fix

--- cron.php
+++ cron.php
@@ -4,7 +4,8 @@ require_once(ABSPATH . 'wp-admin/includes/media.php');
 require_once(ABSPATH . 'wp-admin/includes/file.php');
 require_once(ABSPATH . 'wp-admin/includes/image.php');
 
-//file_get_contents("https://api.telegram.org/bot5809391264:AAG4PGosuFIc7MnYLx6xteUw-j893H9QAlM/sendMessage?chat_id=114785662&text=cronworking!");
+// You can check if the cron is working by uncommenting the following line
+//file_get_contents("https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text=CRON WORKING");
 
 // get all rss
 $rss_list = getAllRss();
